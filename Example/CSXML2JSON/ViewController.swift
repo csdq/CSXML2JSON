a5bade326bb6d4d11ca56db0a3c3e5f21ccbad4d
Mr.s <stqemail@163.com>
2018-12-13 12:34:31 +0800

    update example & update README

--- Example/CSXML2JSON/ViewController.swift
+++ Example/CSXML2JSON/ViewController.swift
@@ -15,30 +15,28 @@ class ViewController: UIViewController {
     let tool : CSXML2JSON = CSXML2JSON.init()
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.textView.isEditable = false
-        tool.xml2jsonObject(xml:
-"""
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE note SYSTEM "Note.dtd">
-<!-- Comments -->
-<cs:books groupId='123' xmlns:cs="http://xxxx.com">
-<book>
-<author id='1'>Tom</author>
-<title>The Apple<a>a tag</a> ^_^ </title>
-<publisher>O'Reilly</publisher>
-</book>
-<book>
-<author id='2'><id>13</id>Jack</author>
-<title>The sheep</title>
-<publisher><![CDATA[ O'Reilly ]]><![CDATA[ Publisher ]]></publisher>
-</book>
-</cs:books>
-"""
+        tool.jsonObject(xml:
+            """
+            <?xml version="1.0" encoding="UTF-8"?>
+            <!DOCTYPE note SYSTEM "Note.dtd">
+            <!-- Comments -->
+            <cs:books groupId='123' xmlns:cs="http://xxxx.com">
+            <book>
+            <author id='1'>Tom</author>
+            <title>The Apple</title>
+            <publisher>O'Reilly</publisher>
+            </book>
+            <book>
+            <author id='2'><id>13</id>Jack</author>
+            <title>The sheep</title>
+            <publisher><![CDATA[ O'Reilly ]]><![CDATA[ Publisher ]]></publisher>
+            </book>
+            </cs:books>
+            """
         ) { (dict, error) in
             do{
                 let json = String.init(data: try JSONSerialization.data(withJSONObject: dict ?? [:], options: JSONSerialization.WritingOptions.prettyPrinted), encoding: String.Encoding.utf8);
                 self.textView.text = json
-                //");
             }catch{
                 
             }
